ac6e26bb330d4203128bad654734d46a495f047c
alban <albankrauth@yahoo.fr>
2022-12-26 01:23:49 +0100

    clean up: put navigation drawer in separate .kv file

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
-from kivy.lang import Builder
-
 from kivy.core.window import Window
+from kivymd.uix.navigationdrawer import MDNavigationDrawer
 
 Window.size = (450, 800)  # to see how it looks in portrait mode
 
 
 class MyScreen(Screen):
+    pass
+
+
+class WuerflerNavigationDrawer(MDNavigationDrawer):
     pass
 
 
